2badc3de5496953d74214ce5f87e2f2fcb52e326
Ruben Lopez <ruben.lopez.perez@gmail.com>
2025-10-22 13:24:35 +0200

    docs(readme): update README.md and refactor code to address SonarQube code smells
    Updated project README
    
    Refactored minor code smells identified by SonarQube (maintainability improvements)
    
    Ensured documentation alignment with current Quality Gate standards

--- namespace/tools/texts.ts
+++ namespace/tools/texts.ts
@@ -6,9 +6,9 @@ export class ToolsTexts {
       if (!str) return '';
 
 const g = str
-  .replaceAll(/\s*\(.*?\)\s*/g, '') // elimina cualquier "(...)" con espacios
-  .replaceAll('\n', ' ')            // reemplaza saltos de línea por espacios
-  .replaceAll(/\s+/g, ' ')          // colapsa múltiples espacios en uno
+        .replaceAll(/\s*\([^()]*\)/g, '') // elimina cualquier "(...)" con espacios
+        .replaceAll('\n', ' ')            // reemplaza saltos de línea por espacios
+        .replaceAll(/\s+/g, ' ')          // colapsa múltiples espacios en uno
   .trim();
 
 
@@ -17,3 +17,4 @@ const g = str
    
   
 }
+
